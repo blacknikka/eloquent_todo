573dbfce08c0d13a691edbcafe61224ef2c75b1c
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2019-08-15 19:30:36 +0900

    Modify initial shell

--- etc/init-project.sh
+++ etc/init-project.sh
@@ -1,11 +1,11 @@
 # envのコピー
-cat ./laradock/env-example ./etc/laradock.env-example > ./laradock/.env
-cp ./etc/laravel.env.sample ./src/.env
+cat ./laradock/env-example ./etc/env-example > ./laradock/.env
+cp ./etc/.env.example ./src/.env
 
 # create database.
 cp ./etc/grant-all-to-testing-database.sql ./laradock/mysql/docker-entrypoint-initdb.d
 
-# # docker up
+# docker up
 pushd ./laradock
 docker-compose build --no-cache nginx mysql workspace
 docker-compose up -d nginx mysql workspace
