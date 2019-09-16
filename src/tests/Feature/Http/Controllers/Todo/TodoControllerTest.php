da0e1c0a0d861eed32a9f655dac36687ba5b192d
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-09-16 20:47:21 +0900

    Add feature test for controller.

--- src/tests/Feature/Http/Controllers/Todo/TodoControllerTest.php
+++ src/tests/Feature/Http/Controllers/Todo/TodoControllerTest.php
@@ -5,12 +5,53 @@ namespace Tests\Feature\Http\Controllers\Todo;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\Todo\TodoRepository;
+use App\Models\Todo\Todo;
+use App\Models\User\UserId;
+use Mockery;
 
 class TodoControllerTest extends TestCase
 {
+    /** @var Mockery\MockInterface */
+    private $todoRepositoryMock;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // 依存をモックする
+        $this->todoRepositoryMock = Mockery::mock(TodoRepository::class);
+
+        // 注入
+        app()->instance(TodoRepository::class, $this->todoRepositoryMock);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        // close
+        Mockery::close();
+    }
+
     /** @test */
     public function get正常系()
     {
+        $this->todoRepositoryMock
+            ->shouldReceive('getTodosByUserId')
+            ->once()
+            ->andReturn(
+                collect(
+                    [
+                        new Todo(
+                            new UserId(1),
+                            'comment',
+                            'title'
+                        )
+                    ]
+                )
+            );
+
         $response = $this->getJson(
             route(
                 'getTodo',
@@ -21,5 +62,13 @@ class TodoControllerTest extends TestCase
         );
 
         $response->assertStatus(200);
+
+        $this->todoRepositoryMock
+            ->shouldHaveReceived('getTodosByUserId')
+            ->with(
+                \Hamcrest\Matchers::equalTo(
+                    new UserId(1)
+                )
+            );
     }
 }
